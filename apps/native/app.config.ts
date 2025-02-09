401d5bf4ea711a8fe8848a678c4ccfeacd9a3067
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-02-09 08:09:37 +0200

    chore: add native app's config

---
+++ apps/native/app.config.ts
@@ -0,0 +1,44 @@
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "Sinagram",
+  slug: "sg-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#000000",
+    },
+    package: "com.alghoul.sinagram",
+  },
+  scheme: "sinagram.ag",
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+};
+
+export default config;
