ecd190d242ce0918a2a6c32cd3d62bb156909231
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-02-12 19:50:32 +0200

    feat: add TRPCProvider

---
+++ apps/native/components/ui/core/trpc/TRPCProvider.tsx
@@ -0,0 +1,83 @@
+import { getQueryClient, trpc } from "@repo/api/client";
+import { useState } from "react";
+import { httpBatchLink, retryLink } from "@trpc/client";
+import superjson from "superjson";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { getJWT, useSession } from "@/components/AuthContext";
+
+export function TRPCNativeProvider(
+  props: Readonly<{
+    children: React.ReactNode;
+  }>,
+) {
+  // NOTE: Avoid useState when initializing the query client if you don't
+  //       have a suspense boundary between this and the code that may
+  //       suspend because React will throw away the client on the initial
+  //       render if it suspends and there is no boundary
+  const queryClient = getQueryClient();
+  const { logOut, logIn } = useSession();
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        retryLink({
+          // @ts-ignore this has to be async
+          async retry(opts) {
+            if (opts.error.data && opts.error.data.code === "UNAUTHORIZED") {
+              const session = getJWT();
+              const refreshResponse = await fetch(
+                "http://192.168.1.19:3000/api/auth/refresh",
+                {
+                  method: "POST",
+                  credentials: "include",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    refreshToken: session?.refreshToken,
+                  }),
+                },
+              );
+
+              if (!refreshResponse.ok || !session) logOut();
+
+              const newSessionData = await refreshResponse.json();
+              const deserialized = superjson.parse(superjson.stringify(newSessionData.data));
+              logIn(deserialized as JWTPayload);
+              return true;
+            }
+            if (
+              opts.error.data &&
+              opts.error.data.code !== "INTERNAL_SERVER_ERROR"
+            ) {
+              // Don't retry on non-500s
+              return false;
+            }
+            if (opts.op.type !== "query") {
+              // Only retry queries
+              return false;
+            }
+            // Retry up to 3 times
+            return opts.attempts <= 3;
+          },
+        }),
+        httpBatchLink({
+          transformer: superjson,
+          url: "http://192.168.1.19:3000/api/trpc",
+          async headers() {
+            const session = getJWT();
+            return {
+              Authorization: session?.accessToken,
+            };
+          },
+        }),
+      ],
+    }),
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
