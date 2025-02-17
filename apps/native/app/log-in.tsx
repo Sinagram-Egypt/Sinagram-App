cb218bd8a632428c72efee5dff77f0f47a7da601
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-02-17 12:44:21 +0200

    feat(native): add linear gradient background

--- apps/native/app/log-in.tsx
+++ apps/native/app/log-in.tsx
@@ -5,37 +5,43 @@ import Register from "@/components/ui/core/user/Register";
 import { useState } from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function SignIn() {
   const [activeTab, setActiveTab] = useState<"login" | "register">("login");
 
   return (
-    <SafeAreaView className="flex-1 justify-center bg-background">
-      <Card className="border border-gray-400 mx-4 gap-4">
-        <View className="flex-row gap-3 mx-4">
-          <InputToggle
-            title="Register"
-            activeTab={activeTab}
-            setActiveTab={() => setActiveTab("register")}
-          />
-          <InputToggle
-            title="Login"
-            activeTab={activeTab}
-            setActiveTab={() => setActiveTab("login")}
-          />
-        </View>
+    <LinearGradient
+      colors={["#3B21B7", "#8B64DA", "#D195EE", "#CECBD3"]}
+      className="flex-1"
+    >
+      <SafeAreaView className="flex-1 justify-center">
+        <Card className="border border-gray-400 mx-4 gap-4 bg-[#D195EE]">
+          <View className="flex-row gap-3 mx-4">
+            <InputToggle
+              title="Register"
+              activeTab={activeTab}
+              setActiveTab={() => setActiveTab("register")}
+            />
+            <InputToggle
+              title="Login"
+              activeTab={activeTab}
+              setActiveTab={() => setActiveTab("login")}
+            />
+          </View>
 
-        <Text className="text-2xl ml-2">
-          {activeTab === "register" ? "Register" : "Login"}
-        </Text>
+          <Text className="text-2xl ml-2">
+            {activeTab === "register" ? "Register" : "Login"}
+          </Text>
 
-        {activeTab === "register" ? (
-          <Register onSuccess={() => setActiveTab("login")} />
-        ) : null}
+          {activeTab === "register" ? (
+            <Register onSuccess={() => setActiveTab("login")} />
+          ) : null}
 
-        {activeTab === "login" ? <Login /> : null}
-      </Card>
-    </SafeAreaView>
+          {activeTab === "login" ? <Login /> : null}
+        </Card>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
