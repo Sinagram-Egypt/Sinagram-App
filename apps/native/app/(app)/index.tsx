0aab61e1069e96025e0914e909aff7b109e9c5e7
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2025-02-17 12:45:39 +0200

    feat(native): add linear gradient background

--- apps/native/app/(app)/index.tsx
+++ apps/native/app/(app)/index.tsx
@@ -1,20 +1,22 @@
 import { useSession } from "@/components/AuthContext";
 import { Button, ButtonText } from "@/components/ui/button";
-import { trpc } from "@repo/api/client";
-import { Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   const { logOut } = useSession();
-  const hello = trpc.hello.useQuery();
 
   return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>Home</Text>
-      <Button onPress={logOut}>
-        <ButtonText>Log out</ButtonText>
-      </Button>
-      <Text>{hello.data?.hello}</Text>
-    </SafeAreaView>
+    <LinearGradient
+      colors={["#3B21B7", "#8B64DA", "#D195EE", "#CECBD3"]}
+      className="flex-1"
+    >
+      <SafeAreaView>
+        <Button onPress={logOut}>
+          <ButtonText>Log out</ButtonText>
+        </Button>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
